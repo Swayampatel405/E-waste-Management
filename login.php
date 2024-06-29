104fe808dc2480d0916ca7bc09411fd96cadde06
Swayampatel405 <swyampatel405@gmail.com>
2024-06-29 19:39:36 +0530

    Commit Final Website

--- login.php
+++ login.php
@@ -33,7 +33,7 @@ if(!empty($_SESSION["id"])){
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Donate Food</title>
+    <title>Log In</title>
     <link rel="icon" href="./files/logo.png">
     <link rel="stylesheet" type="text/css" href="css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
